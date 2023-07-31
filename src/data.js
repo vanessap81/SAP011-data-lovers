5b7f509f128f86a51cefdd73627e49bd5cdf80f2
Vanessa Ponte <vanessaponte@gmail.com>
2023-07-31 10:57:50 -0300

    Testes de filtro

--- src/data.js
+++ src/data.js
@@ -1,9 +1,25 @@
 // estas funciones son de ejemplo
 
 export const example = () => {
-  return 'example';
+  return "example";
 };
 
 export const anotherExample = () => {
-  return 'OMG';
+  return "OMG";
+};
+
+export const filterData = () => {
+  const arrayGot = Object.values();
+  const targaryenFilter = arrayGot.filter(
+    (targaryenFamily) => targaryenFamily.family === "House Targaryen"
+  );
+  targaryenFilter.forEach((targaryenFamily) => {
+    console.log(targaryenFamily);
+  });
+};
+export const sortData = () => {
+  return "OMG";
+};
+export const computeData = () => {
+  return "OMG";
 };
