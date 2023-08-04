e0cd403a9856fd3a7cfff955dd7b600f75846399
jumvribeiro <juliamvribeiro@gmail.com>
2023-08-03 21:06:08 -0300

    testando o filtro buscar por nome

--- src/data.js
+++ src/data.js
@@ -1,26 +1,4 @@
-// estas funciones son de ejemplo
-
-// export const example = () => {
-//   return "example";
-// };
-
-// export const anotherExample = () => {
-//   return "OMG";
-// };
-
-export const filterData = () => {
-  const arrayGot = Object.values();
-  const targaryenFilter = arrayGot.filter(
-    (targaryenFamily) => targaryenFamily.family === "House Targaryen"
-  );
-  targaryenFilter.forEach((targaryenFamily) => {
-    console.log(targaryenFamily);
-  });
+export const nomesFiltro = (dados, nomeBuscar) => {
+  return dados.filter(personagem => personagem.name === nomeBuscar);
 };
 
-export const sortData = () => {
-  return "OMG";
-};
-export const computeData = () => {
-  return "OMG";
-};
--- src/main.js
+++ src/main.js
@@ -1,30 +1,59 @@
 // import { example } from "./data.js";
 // console.log(example, data);
 
+import { nomesFiltro } from "./data.js";
 import data from "./data/got.js";
 
 // const orderSelector = document.getElementById("order-selector");
 // const familySelector = document.getElementById("family-selector");
 // const nameSearch = document.getElementById("name-search");
 // const percent = document.getElementById("statistics");
-const cardsContainer = document.getElementById("cards-container");
-const got = data.got;
+// const cardsContainer = document.getElementById("cards-container");
+//const got = data.got;
 
-const Cards = (data) => {
-  cardsContainer.innerHTML = data
-    .map(
-      (got) =>
-        `
-        <div class="card">
-            <h3 class="fullname-got"> ${got.fullName}</h3>
-            <img class="image-got" src=${got.imageUrl} />
-            <div class="content-card">
-                <p><strong>Título:</strong> ${got.title}</p>
-                <p><strong>Família:</strong> ${got.family}</p>
-                <p><strong>Nascido em:</strong> ${got.born}</p>
-            </div>
-        </div>
-        `
-    )
-    .join("");
-};
+// const Cards = (data) => {
+//  cardsContainer.innerHTML = data
+//    .map(
+//      (got) =>
+//        `
+//        <div class="card">
+//           <h3 class="fullname-got"> ${got.fullName}</h3>
+//            <img class="image-got" src=${got.imageUrl} />
+//            <div class="content-card">
+//                <p><strong>Título:</strong> ${got.title}</p>
+//                <p><strong>Família:</strong> ${got.family}</p>
+//                <p><strong>Nascido em:</strong> ${got.born}</p>
+//            </div>
+//        </div>
+//        `
+//    )
+//    .join("");
+//};
+
+// formato de objeto para transformar em formato de array para facilitar a manipulação
+const dadosGot = Object.values(data);
+
+const conteudo = document.getElementById ("cards-container");
+function cardsTela (data) {
+  conteudo.innerHTML = data.map((got) => `
+  <div class="card">
+      <h3 class="fullname-got">${got.fullName}</h3>
+      <img class="image-got" src=${got.imageUrl} />
+      <div class="content-card">
+        <p><strong>Título:</strong> ${got.title}</p>
+        <p><strong>Família:</strong> ${got.family}</p>
+        <p><strong>Nascido em:</strong> ${got.born}</p>
+      </div>
+    </div>
+  `).join('');
+}
+cardsTela(dadosGot);
+
+
+
+const campoBuscar = document.getElementById("name-search");
+campoBuscar.addEventListener("input", event => {
+  const nomeDoPersonagem = event.target.value;
+  const filtrarPersonagem = nomesFiltro(dadosGot, nomeDoPersonagem);
+  cardsTela(filtrarPersonagem);
+});
