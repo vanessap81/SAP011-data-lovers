3bc4976aeae051adc51ff22111d5facd97dcfcf9
jumvribeiro <juliamvribeiro@gmail.com>
2023-08-07 12:58:05 -0300

    testes buscar

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
 export const nomesFiltro = (dados, nomeBuscar) => {
-  return dados.filter(personagem => personagem.name === nomeBuscar);
+  return dados.filter(personagem => personagem.name.toUpperCase().includes(nomeBuscar.toUpperCase()))
 };
 
--- src/main.js
+++ src/main.js
@@ -4,32 +4,6 @@
 import { nomesFiltro } from "./data.js";
 import data from "./data/got.js";
 
-// const orderSelector = document.getElementById("order-selector");
-// const familySelector = document.getElementById("family-selector");
-// const nameSearch = document.getElementById("name-search");
-// const percent = document.getElementById("statistics");
-// const cardsContainer = document.getElementById("cards-container");
-//const got = data.got;
-
-// const Cards = (data) => {
-//  cardsContainer.innerHTML = data
-//    .map(
-//      (got) =>
-//        `
-//        <div class="card">
-//           <h3 class="fullname-got"> ${got.fullName}</h3>
-//            <img class="image-got" src=${got.imageUrl} />
-//            <div class="content-card">
-//                <p><strong>Título:</strong> ${got.title}</p>
-//                <p><strong>Família:</strong> ${got.family}</p>
-//                <p><strong>Nascido em:</strong> ${got.born}</p>
-//            </div>
-//        </div>
-//        `
-//    )
-//    .join("");
-//};
-
 // formato de objeto para transformar em formato de array para facilitar a manipulação
 const dadosGot = Object.values(data.got);
 
@@ -39,17 +13,16 @@ function cardsTela (data) {
   <div class="card">
       <h3 class="fullname-got"> ${got.fullName}</h3>
       <img class="image-got" src="${got.imageUrl}"/>
-      <div class="content-card">
-        <p><strong>Título:</strong> ${got.title}</p>
-        <p><strong>Família:</strong> ${got.family}</p>
-        <p><strong>Nascido em:</strong> ${got.born}</p>
+
+      <div id="p-wrap">
+          <p><strong>Título: </strong>${got.title}</p>
+          <p><strong>Família: </strong>${got.family}</p>
+          <p><strong>Nascido em: </strong>${got.born}</p>
       </div>
     </div>
   `).join ('');
 }
 cardsTela(dadosGot);
-
-
 
 const campoBuscar = document.getElementById("name-search");
 campoBuscar.addEventListener("input", event => {
