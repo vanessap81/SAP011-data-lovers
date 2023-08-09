898ee02fabb8fb30eee786ec3b701f5644f89402
Vanessa Ponte <vanessaponte@gmail.com>
2023-08-09 17:45:18 -0300

    Updates

--- main.js
+++ main.js
@@ -53,3 +53,8 @@ searchPersonagem.addEventListener("input", (event) => {
   const buscarPersonagem = filterNomes(got, nomePersonagem);
   fillContainer(buscarPersonagem);
 });
+
+const btnTodosPersonagem = document.getElementById("botao-allcards");
+btnTodosPersonagem.addEventListener("click", function () {
+  location.reload();
+});
