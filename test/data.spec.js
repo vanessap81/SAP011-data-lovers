714a31e674dba519c902190dd5d46bdba27c3258
Vanessa Ponte <vanessaponte@gmail.com>
2023-08-10 15:46:44 -0300

    teste para funções

--- test/data.spec.js
+++ test/data.spec.js
@@ -2,30 +2,30 @@ import { sortData, filterData, computeStats } from "../src/data.js";
 
 describe("sortData", () => {
   it("is a function", () => {
-    expect(typeof example).toBe("function");
+    expect(typeof sortData).toBe("function");
   });
 
-  it("returns `example`", () => {
-    expect(example()).toBe("example");
-  });
+  // it("returns `example`", () => {
+  //   expect(example()).toBe("example");
+  // });
 });
 
 describe("filterData", () => {
   it("is a function", () => {
-    expect(typeof anotherExample).toBe("function");
+    expect(typeof filterData).toBe("function");
   });
 
-  it("returns `anotherExample`", () => {
-    expect(anotherExample()).toBe("OMG");
-  });
+  // it("returns `anotherExample`", () => {
+  //   expect(anotherExample()).toBe("OMG");
+  // });
 });
 
 describe("computeStats", () => {
   it("is a function", () => {
-    expect(typeof anotherExample).toBe("function");
+    expect(typeof computeStats).toBe("function");
   });
 
   it("returns `anotherExample`", () => {
-    expect(anotherExample()).toBe("OMG");
+    expect(computeStats("40")).toBe("75.4");
   });
 });
